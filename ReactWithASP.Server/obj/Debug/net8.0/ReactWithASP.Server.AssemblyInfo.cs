58dd370f64500881b7ac4b4d62b638680b6e6758
valdas <valdas.kriunas@panko.lt>
2024-10-14 19:04:13 +0300

    Trying to merge branches

--- ReactWithASP.Server/obj/Debug/net8.0/ReactWithASP.Server.AssemblyInfo.cs
+++ ReactWithASP.Server/obj/Debug/net8.0/ReactWithASP.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactWithASP.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e58c55fe0c49d988451c1dd4f3fd0327c771871a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactWithASP.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactWithASP.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
