4445f143bd48c5db2f33b1c061ed07ee8565ce56
valdas <valdas.kriunas@panko.lt>
2024-10-23 20:18:55 +0300

    Finished admin panel

--- ReactWithASP.Server/Controllers/Admin/DashboardController.cs
+++ ReactWithASP.Server/Controllers/Admin/DashboardController.cs
@@ -3,9 +3,21 @@
 [ApiController]
 [Route("api/admin/[controller]")]
 [Authorize(Roles = UserRoles.Admin)]
-public class DashboardController() : ControllerBase
+public class DashboardController(UserManager<IdentityUser> userManager) : ControllerBase
 {
-    [HttpGet]
-    public IActionResult Show() => Ok(new { text = "You logged to dashboard!" });
+    //[HttpGet]
+    //public IActionResult Show() => Ok(new { text = "You logged to dashboard!" });
 
+
+    [HttpGet]
+    public async Task<List<IdentityUser>> GetAllUsers()
+    {
+        var users = userManager.Users.ToListAsync();
+        List<IdentityUser> results = [];
+        foreach (var user in await users)
+        {
+            results.Add(user);
+        }
+        return results;
+    }
 }
--- reactwithasp.client/src/pages/admin/DashboardPage/Dashboard.tsx
+++ reactwithasp.client/src/pages/admin/DashboardPage/Dashboard.tsx
@@ -1,6 +1,8 @@
 import useSWR from 'swr';
 import { getApi } from "@/api";
 import { IDashboard } from "@/interfaces/IDashboard";
+import { useEffect, useState } from 'react';
+import { IUser } from '../../../interfaces/IUser';
 
 export default function Dashboard() {
     const { data, error, isLoading } = useSWR<IDashboard | undefined>(
@@ -10,9 +12,22 @@ export default function Dashboard() {
     }
     );
 
+    const [users, setUsers] = useState<IUser[]>([])
+    const getUsers = () => getApi<IUser[]>('admin/dashboard').then(s => s && setUsers(s))
+
+    useEffect(() => {
+        getUsers().then(i => 1)
+    }, [])
+
     return <div>
         <h1 className="text-xl text-blue-950"> Admin Dashboard</h1>
         {error ? <div>{error} </div> : null}
         {data?.text}
+
+        <ul>
+            {users.map(user => (
+                <li>{user.userName} {user.email}</li>
+            ))}
+        </ul>
     </div>
 }
